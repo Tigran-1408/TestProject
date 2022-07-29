d98ab4fe3614cf6938a8f2ff6a369f34b5e6e2fb
Tigran Babayan <deema1900@outlook.com>
2022-07-29 15:40:03 +0400

    AddValidators

--- src/WebUI/ClientApp/src/app/home/home.component.ts
+++ src/WebUI/ClientApp/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import {NgModel} from "@angular/forms";
+import {Component, OnDestroy} from '@angular/core';
+import {FormControl, FormGroup, NgModel, Validators} from "@angular/forms";
+import { Subscription } from "rxjs";
 
 export class Phone{
   constructor(public title: string,
@@ -12,13 +13,30 @@ export class Phone{
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy{
   title: string = "";
   price: number = 0;
   company: string = "";
+  form : FormGroup;
+  protected _subs: Subscription = new Subscription();
+
 
   phone = new Phone("",0,"");
   companies: string[] = ["Apple", "Huawei", "Xiaomi", "Samsung", "LG", "Motorola", "Alcatel"];
+
+
+  // constructor(){
+  //   this.form = new FormGroup({
+  //     "userName": new FormControl("Tom", Validators.required),
+  //     "userEmail": new FormControl("", [
+  //       Validators.required,
+  //       Validators.email
+  //     ]),
+  //     "userPhone": new FormControl("", Validators.pattern("[0-9]{10}"))
+  //   });
+  // }
+
+  //validation
 
   addPhone(title:NgModel, price: NgModel, comp: NgModel){
     console.log(title);
@@ -26,8 +44,22 @@ export class HomeComponent {
     console.log(comp);
   }
 
+  ngOnDestroy() {
+    this._subs.unsubscribe();
+  }
+  ngOnInit(){
+
+
+  }
+
+
+
   onTitleChange(){
     if(this.phone.title=="нет")
       this.phone.title = "неизвестно";
   }
+
+
+
+
 }
